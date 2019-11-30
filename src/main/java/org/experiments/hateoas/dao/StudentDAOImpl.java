1ae7d72fb02ec48ca4f5bc5c85041b74b91c00a6
msoumik78 <msoumik78@gmail.com>
2019-12-01 03:30:53 +0530

    Update StudentDAOImpl.java

--- src/main/java/org/experiments/hateoas/dao/StudentDAOImpl.java
+++ src/main/java/org/experiments/hateoas/dao/StudentDAOImpl.java
@@ -12,8 +12,16 @@ public class StudentDAOImpl implements IStudentDAO{
     @Override
     public List<Student> getAllStudents() {
         List<Student> studentList = new ArrayList<>();
-        Student firstStudent = Student.builder().studentName("Soumik").courseName("Advanced Java").courseDurationInMonths(6).build();
+        Student firstStudent = Student.builder().studentName("Soumik1").courseName("Advanced Java").courseDurationInMonths(6).build();
+        Student secondStudent = Student.builder().studentName("Soumik2").courseName("REST").courseDurationInMonths(6).build();
+        Student thirdStudent = Student.builder().studentName("Soumik3").courseName("Clean Code").courseDurationInMonths(6).build();
+        Student fourthStudent = Student.builder().studentName("Soumik4").courseName("Graal VM").courseDurationInMonths(6).build();
+        Student fifthStudent = Student.builder().studentName("Soumik5").courseName("Spring Boot").courseDurationInMonths(6).build();
         studentList.add(firstStudent);
+        studentList.add(secondStudent);
+        studentList.add(thirdStudent);
+        studentList.add(fourthStudent);
+        studentList.add(fifthStudent);
         return studentList;
     }
 }
